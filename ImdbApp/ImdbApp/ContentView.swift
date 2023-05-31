1ae141132c64a35f2543ab23f66533bfabc722d7
kerimgaydan <mkerimgaydan@gmail.com>
2023-05-31 16:19:28 +0300

    deneme 2

--- ImdbApp/ImdbApp/ContentView.swift
+++ ImdbApp/ImdbApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hellsdasdasasdsadao, world!")
+            Text("Hello, world!")
         }
         .padding()
     }
