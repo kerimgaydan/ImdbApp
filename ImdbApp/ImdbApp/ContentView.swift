117017764b64b1f3aeaaf8168cad6ccfd6a96a8d
kerimgaydan <116214457+kerimgaydan@users.noreply.github.com>
2023-05-31 16:16:43 +0300

    Add files via upload
    sjdhgfkjs

---
+++ ImdbApp/ImdbApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImdbApp
+//
+//  Created by Kerim Gaydan on 31.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hellsdasdasao, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImdbApp/ImdbApp/ImdbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImdbAppApp.swift
+//  ImdbApp
+//
+//  Created by Kerim Gaydan on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ImdbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
